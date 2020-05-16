01a5c82b68ac3da8e75390796cfa5c5c0e66dba3
Freddy_Castro <jhonfreddycastrom@gmail.com>
2020-05-16 15:51:05 -0500

    Java FX

---
+++ src/Controlador/PrincipalController.java
@@ -0,0 +1,16 @@
+package Controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class PrincipalController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Vista/Principal.java
@@ -0,0 +1,27 @@
+package Vista;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Principal extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/Principal.fxml"));
+		
+		Parent p = loader.load();
+		Scene s = new Scene(p);
+		
+		primaryStage.setScene(s);
+		primaryStage.show();
+	}
+	
+	public static void main(String args[]) {
+		launch(args);
+	}
+
+}
